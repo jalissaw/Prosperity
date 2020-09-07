1d09af97fd0891678f206f58b42324757749dcbc
Jalissa Williams <jalissa_williams@yahoo.com>
2020-09-07 18:55:54 -0400

    added menu functions

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
-const toggle = document.querySelector('.toggle');
+const toggler = document.querySelector('.toggle');
 const showMenu = document.querySelector('.showmenu');
 const links = document.querySelectorAll('.showlink');
+const aLinks = document.querySelectorAll('.showlink a');
 
 
-toggle.addEventListener('click', () => {
+
+// toggle function on click of menu
+toggler.addEventListener('click', () => {
     for (let i = 0; i < links.length; i++) {
         setTimeout(function () {
-            links[i].classList.toggle("appear");
+            links[i].classList.add("appear");
         }, 150 * i);
     }
     showMenu.classList.toggle("appear")
 })
 
+
+//remove links and menu when click on link
+function removeMenu() {
+    for (let i = 0; i < aLinks.length; i++) {
+        aLinks[i].addEventListener('click', () => {
+            showMenu.classList.remove('appear')
+            links[i].classList.remove("appear");
+        })
+    }
+}
+
+removeMenu()
 
